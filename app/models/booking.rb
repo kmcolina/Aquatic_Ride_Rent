0507c65bcd7b909a07fb110d7eb9583dafff081d
luiszambrano <luisdavidzambrano@gmail.com>
2021-10-27 15:39:05 -0400

    completado_crud

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,5 +1,5 @@
 class Booking < ApplicationRecord
   belongs_to :user
-  belongs_to :objeto_acuatico, dependent: :destroy
+  belongs_to :objeto_acuatico
   validates :checkin, :checkout, :capacity, presence: true
 end
--- app/models/objeto_acuatico.rb
+++ app/models/objeto_acuatico.rb
@@ -1,6 +1,6 @@
 class ObjetoAcuatico < ApplicationRecord
   belongs_to :user
-
+  has_many :bookings, dependent: :destroy
   has_one_attached :photo
 
   validates :title, :location, :price, :capacity, presence: true
