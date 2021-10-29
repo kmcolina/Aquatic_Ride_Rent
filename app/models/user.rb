534c1f1a103708ff8e5bc672854313e60cf5242e
katherine colina <kathymcolina@gmail.com>
2021-10-29 15:09:37 -0400

    image profile

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   has_many :bookings
   has_many :objeto_acuaticos
+  has_one_attached :avatar
 
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
